4a646deb64543429c18c99c074981c051a10d860
ghembazailyas <ghembazailyas@gmail.com>
2013-11-05 11:45:42 +0000

    Package Modèle complet

---
+++ Gsb_medecin/src/java/modele/Test.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+/**
+ *
+ * @author ighembaza
+ */
+public class Test {
+    public static void main(String[] args) {
+	Pays pays= new Pays();
+	System.out.println ("");
+    }
+}
